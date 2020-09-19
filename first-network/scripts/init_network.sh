92b11730d081d22bf356db8f1388854465bfaea8
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2020-09-19 00:22:31 -0700

    Modify configtx.yaml and automate network start

--- first-network/scripts/init_network.sh
+++ first-network/scripts/init_network.sh
@@ -1,5 +1,54 @@
 export PATH=${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=$PWD
 
-# cert
+# create cert file
 ../bin/cryptogen generate --config=./crypto-config.yaml
+
+# create genesis block
+../bin/configtxgen -profile OrdererGenesis \
+-channelID byfn-sys-channel \
+-outputBlock ./channel-artifacts/genesis.block
+
+function createChannelTx() {
+    export CHANNEL_NAME=$1
+
+    ../bin/configtxgen -profile $2 \
+    -outputCreateChannelTx ./channel-artifacts/$3 \
+    -channelID $CHANNEL_NAME
+}
+
+function udpateAnchorPeer() {
+    ../bin/configtxgen -profile $1 \
+    -outputAnchorPeersUpdate ./channel-artifacts/$2 \
+    -channelID $CHANNEL_NAME \
+    -asOrg $3
+}
+
+# create department channel and update anchor peer
+createChannelTx itchannel ITChannel itchannel.tx
+udpateAnchorPeer ITChannel OrgITMSPanchors.tx OrgITMSP
+
+createChannelTx cechannel CEChannel cechannel.tx
+udpateAnchorPeer CEChannel OrgCEMSPanchors.tx OrgCEMSP
+
+createChannelTx korchannel KORChannel korchannel.tx
+udpateAnchorPeer KORChannel OrgKORMSPanchors.tx OrgKORMSP
+
+createChannelTx engchannel ENGChannel engchannel.tx
+udpateAnchorPeer ENGChannel OrgENGMSPanchors.tx OrgENGMSP
+
+# create coe, chss channel and update anchor peer
+createChannelTx coechannel COEChannel coechannel.tx
+udpateAnchorPeer COEChannel COEITMSPanchors.tx OrgITMSP
+udpateAnchorPeer COEChannel COECEMSPanchors.tx OrgCEMSP
+
+createChannelTx chsschannel CHSSChannel chsschannel.tx
+udpateAnchorPeer CHSSChannel CHSSKORMSPanchors.tx OrgKORMSP
+udpateAnchorPeer CHSSChannel CHSSENGMSPanchors.tx OrgENGMSP
+
+# create college channel and update anchor peer
+createChannelTx collegechannel CollegeChannel collegechannel.tx
+udpateAnchorPeer CollegeChannel CollegeITMSPanchors.tx OrgITMSP
+udpateAnchorPeer CollegeChannel CollegeCEMSPanchors.tx OrgCEMSP
+udpateAnchorPeer CollegeChannel CollegeKORMSPanchors.tx OrgKORMSP
+udpateAnchorPeer CollegeChannel CollegeENGMSPanchors.tx OrgENGMSP
