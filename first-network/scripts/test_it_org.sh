712982019f0b3b628574529c5ed62853334cd4f4
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2020-09-20 02:20:10 -0700

    Apply sample test code

--- first-network/scripts/test_it_org.sh
+++ first-network/scripts/test_it_org.sh
@@ -32,3 +32,29 @@ peer channel update -o orderer.evote.com:7050 \
 --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/evote.com/orderers/orderer.evote.com/msp/tlscacerts/tlsca.evote.com-cert.pem
 
 echo "OrgITMSP anchor updated"
+
+# install chaincode
+echo "start install chaincode"
+
+CORE_PEER_LOCALMSPID="OrgITMSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/itcae.evote.com/peers/peer0.itcae.evote.com/tls/ca.crt
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/itcae.evote.com/users/Admin@itcae.evote.com/msp
+CORE_PEER_ADDRESS=peer0.itcae.evote.com:7051
+
+peer chaincode install -n itcc -v 1.0 -p github.com/chaincode/fabcar/go/
+
+echo "chaincode installed"
+
+# instantiate
+echo "start chaincode instantiate"
+
+peer chaincode instantiate -o orderer.evote.com:7050 \
+--tls \
+--cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/evote.com/orderers/orderer.evote.com/msp/tlscacerts/tlsca.evote.com-cert.pem \
+-C $CHANNEL_NAME \
+-n itcc \
+-v 1.0 \
+-c '{"Args":[]}' \
+-P "AND ('OrgITMSP.peer')"
+
+echo "success"
