ee55191820a044b657de1511d7696a4d57aad727
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2020-09-29 01:14:00 -0700

    Test chaincode in one channel

--- first-network/scripts/init_network.sh
+++ first-network/scripts/init_network.sh
@@ -60,28 +60,3 @@ docker ps -a
 docker exec cli scripts/test_it_org.sh
 # docker exec cli scripts/install.sh
 
-# # test chaincode
-# CC_SRC_PATH=github.com/chaincode/fabcar/go
-# CHANNEL_NAME=itchannel
-# CCNAME=itcc
-# Version=1.0
-
-# CORE_PEER_LOCALMSPID="OrgITMSP"
-# CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/itcae.evote.com/peers/peer0.itcae.evote.com/tls/ca.crt
-# CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/itcae.evote.com/users/Admin@itcae.evote.com/msp
-# CORE_PEER_ADDRESS=peer0.itcae.evote.com:7051
-
-# docker exec cli \
-# peer chaincode invoke -o orderer.evote.com:7050 \
-# --tls true \
-# --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/evote.com/orderers/orderer.evote.com/msp/tlscacerts/tlsca.evote.com-cert.pem \
-# -C $CHANNEL_NAME \
-# -n itcc \
-# --peerAddresses peer0.itcae.evote.com:7051 \
-# --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/itcae.evote.com/peers/peer0.itcae.evote.com/tls/ca.crt \
-# -c '{"Args":["initLedger"]}'
-
-# echo "initLedger invoked"
-# sleep 3
-
-# docker exec cli peer chaincode query -C $CHANNEL_NAME -n itcc -c '{"Args":["queryCar","CAR1"]}' 
