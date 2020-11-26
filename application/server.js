5e7abb204cf0dfbae93ce450f93defd8dc800089
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2020-11-26 14:46:33 +0900

    Delete unuse method

--- application/server.js
+++ application/server.js
@@ -48,55 +48,6 @@ app.use('/end_total', end_total);
 app.use('/result', result);
 app.use('/select', select);
 
-app.get('/api/create', function(req, res) {
-  var title = req.query.title;
-  var org = req.query.org;
-  var channel = req.query.channel;
-  var chaincode = req.query.chaincode;
-  
-  var open = req.query.open;
-  var deadline = req.query.deadline;
-
-  let args = [title, open, deadline];
-  sdk.send(true, org, channel, chaincode, 'create', args, res);
-});
-
-app.get('/api/enroll', function(req, res) {
-  var title = req.query.title;
-  var name = req.query.name;
-  var major = req.query.major;
-  var sid = req.query.sid;
-  var college = req.query.college;
-  var org = req.query.org;
-  var channel = req.query.channel;
-  var chaincode = req.query.chaincode;
-
-  let args = [title, name, major, sid, college]
-  sdk.send(true, org, channel, chaincode, 'enroll', args, res)
-});
-
-app.get('/api/vote', function(req, res) {
-  var title = req.query.title;
-  var from = req.query.from;
-  var to = req.query.to;
-  var org = req.query.org;
-  var channel = req.query.channel;
-  var chaincode = req.query.chaincode;
-
-  let args = [title, from, to];
-  sdk.send(true, org, channel, chaincode, 'vote', args, res);
-});
-
-app.get('/api/query', function(req, res) {
-  var title = req.query.title;
-  var org = req.query.org;
-  var channel = req.query.channel;
-  var chaincode = req.query.chaincode;
-
-  let args = [title];
-  sdk.send(false, org, channel, chaincode, 'query', args, res);
-})
-
 //server start
 app.listen(4000, function(){
   console.log('Connected 4000 port!');
